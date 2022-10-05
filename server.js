903c1eb0f5f2f042863df39754f4cf4b5ebe1853
Shubh17ss <shubhthe2001@gmail.com>
2022-10-05 18:46:04 +0530

    Intitial authors routes

--- server.js
+++ server.js
@@ -4,14 +4,18 @@
 const express=require('express');
 const app=express();
 const expressLayouts=require('express-ejs-layouts')
+const bodyParser=require('body-parser');
+
 
 const indexRouter=require('./routes/index');
+const authorRouter=require('./routes/authors')
 
 app.set('view engine', 'ejs')
 app.set('views',__dirname+'/views')
 app.set('layout', 'layouts/layout')
 app.use(expressLayouts)
 app.use(express.static('public'))
+app.use(bodyParser.urlencoded({limit:'10mb', extended:false}))
 
 const mongoose=require('mongoose');
 mongoose.connect("mongodb://localhost:27017/mybrary").then(()=>{
@@ -21,5 +25,6 @@ mongoose.connect("mongodb://localhost:27017/mybrary").then(()=>{
 })
 
 app.use('/',indexRouter)
+app.use('/authors',authorRouter)
 
 app.listen(process.env.PORT || 3000)
